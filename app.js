88842bd82fab0159416320d666eb396b7db035ca
HatusSouza <86630383+HatusSouza@users.noreply.github.com>
2021-10-01 19:52:51 -0300

    adicionado numero aleatório e seletor de numero de 0 a 10
    http://www.macoratti.net/18/09/js_dif1.htm

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-function Chutar(){
-//sortear um numero aleatorio secreto
-let numeroSecreto = 3
-//pegar o numero digitado e converter para inteiro
-let chute = parseInt( document.querySelector("#valor").value)
-//comparar o numero digitado com o numero aleatorio secreto
-if(numeroSecreto == chute){
-//se tiver correto e os numeros forem iguais,mostrar a menasagem que a pessoa acertou
-document.querySelector(".resultado").innerHTML = "você acertou"
-}
-else{
-//se tiver errado e os numeros forem diferentes , mostrar a mensagem que a pessoa errou.
-document.querySelector(".resultado").innerHTML = "você errou"
-}
 /**
  * Operadores Relacionais
 * < --> menor
@@ -21,6 +7,26 @@ document.querySelector(".resultado").innerHTML = "você errou"
 * == --> igual
 * != --> diferente
 */
+
+function Chutar(){
+//sortear um numero aleatorio secreto
+let numeroSecreto = parseInt(Math.random()*11)
+console.log(numeroSecreto)
+
+//pegar o numero digitado e converter para inteiro
+let chute = parseInt( document.querySelector("#valor").value)
+
+//comparar o numero digitado com o numero aleatorio secreto
+if(numeroSecreto == chute){
+//se tiver correto e os numeros forem iguais,mostrar a menasagem que a pessoa acertou
+document.querySelector(".resultado").innerHTML = "você acertou"
+}
+
+else{
+//se tiver errado e os numeros forem diferentes , mostrar a mensagem que a pessoa errou.
+document.querySelector(".resultado").innerHTML = "você errou"
+}
+
 
 
 
